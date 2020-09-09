dd809f940c6dc32acb5d65d15c9afc560388542d
Lars Giskes <lars.giskes@gmail.com>
2020-09-09 13:56:43 +0200

    [Merge] Merged With GUISimulator
    image has gone missing, but successfully merged

---
+++ Remote Healthcare B3/BikeSimulator/BikeSimulator.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FietsSimulatorGUI
+{
+    public partial class BikeSimulator : Form
+    {
+
+        public int ValuePower { get; set; }
+        public int ValueSpeed { get; set; }
+        public int ValueHeartRate { get; set; }
+
+        public Boolean StartButtonClicked { get; set; }
+
+        public int RandomNumber1 { get; set; }
+        public int RandomNumber2 { get; set; }
+        public int RandomNumber3 { get; set; }
+
+        public Random random { get; set; }
+
+
+        public int Stroom { get; set; }
+
+        public BikeData BikeData { get; set; }
+
+
+        public BikeSimulator()
+        {
+            InitializeComponent();
+            ValuePower = 10;
+            ValueSpeed = 10;
+            ValueHeartRate = 50;
+            StartButtonClicked = false;
+            random = new Random();
+            BikeData = new BikeData(ValueSpeed, ValueHeartRate, ValuePower, 10);
+
+        }
+
+   
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Start geklikt!");
+            Console.WriteLine($"Waardes zijn: {ValuePower}, {ValueSpeed}, {ValueHeartRate}");
+
+
+            ValuePower = (int)numericUpDown1.Value;
+            ValueSpeed = (int)numericUpDown2.Value;
+            ValueHeartRate = (int)numericUpDown3.Value;
+
+            BikeData.Power = ValuePower;
+            BikeData.Speed = ValueSpeed;
+            BikeData.HeartRate = ValueHeartRate;
+
+            StartButtonClicked = true;
+            System.Threading.Thread thread = new System.Threading.Thread(new ThreadStart(WorkThreadFunction));
+            thread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Console.WriteLine("Stop geklikt!");
+            StartButtonClicked = false;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            ValuePower = (int)numericUpDown1.Value;
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            ValueSpeed = (int)numericUpDown2.Value;
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            ValueHeartRate = (int)numericUpDown3.Value;
+
+        }
+
+        public void WorkThreadFunction()
+        {
+            try
+            {
+                while(StartButtonClicked) {
+                    
+
+                    ValuePower = BikeData.GetPower();
+                    ValueSpeed = BikeData.GetSpeed();
+                    ValueHeartRate = BikeData.GetHeartRate();
+
+                    Console.WriteLine($"We are biking with the values  : {ValuePower} W , {ValueSpeed} m/s, {ValueHeartRate} bpm");
+                    Thread.Sleep(1000);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                // log errors
+            }
+        }
+    }
+}
---
+++ Remote Healthcare B3/Simulator/BikeSimulator.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace FietsSimulatorGUI
+{
+    partial class BikeSimulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelfietssimulator = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelfietssimulator
+            // 
+            this.labelfietssimulator.AutoSize = true;
+            this.labelfietssimulator.Font = new System.Drawing.Font("Lucida Sans Unicode", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelfietssimulator.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelfietssimulator.Location = new System.Drawing.Point(25, 41);
+            this.labelfietssimulator.Name = "labelfietssimulator";
+            this.labelfietssimulator.Size = new System.Drawing.Size(592, 78);
+            this.labelfietssimulator.TabIndex = 0;
+            this.labelfietssimulator.Text = "Fiets Simulator B3";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FietsSimulatorGUI.Properties.Resources.FietsIcon;
+            this.pictureBox1.Location = new System.Drawing.Point(597, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(93, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 45);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Stroom(W)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(93, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 45);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Snelheid(m/s)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(93, 358);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(274, 45);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hartslag(bpm)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(520, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 50);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start Simulator";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(101, 195);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Location = new System.Drawing.Point(101, 294);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown3.Location = new System.Drawing.Point(101, 406);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown3.TabIndex = 11;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(520, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 50);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Stop Simulator";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 502);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelfietssimulator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelfietssimulator;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
