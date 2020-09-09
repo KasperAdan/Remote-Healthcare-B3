847ce0489b3f7bdf851d3cbf5e45b971b70b252b
Davy <dj.raitt@student.avans.nl>
2020-09-09 15:19:29 +0200

    Testing push for Jan Kees

--- Remote Healthcare B3/BikeSimulator/BikeSimulator.Designer.cs
+++ Remote Healthcare B3/BikeSimulator/BikeSimulator.Designer.cs
@@ -38,10 +38,15 @@
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             this.SuspendLayout();
             // 
             // labelfietssimulator
@@ -49,7 +54,7 @@
             this.labelfietssimulator.AutoSize = true;
             this.labelfietssimulator.Font = new System.Drawing.Font("Lucida Sans Unicode", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelfietssimulator.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.labelfietssimulator.Location = new System.Drawing.Point(25, 41);
+            this.labelfietssimulator.Location = new System.Drawing.Point(12, 18);
             this.labelfietssimulator.Name = "labelfietssimulator";
             this.labelfietssimulator.Size = new System.Drawing.Size(592, 78);
             this.labelfietssimulator.TabIndex = 0;
@@ -58,7 +63,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::BikeSimulator.Properties.Resources.FietsIcon;
-            this.pictureBox1.Location = new System.Drawing.Point(597, -2);
+            this.pictureBox1.Location = new System.Drawing.Point(646, -1);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(241, 121);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -70,11 +75,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(93, 147);
+            this.label1.Location = new System.Drawing.Point(17, 131);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(206, 45);
+            this.label1.Size = new System.Drawing.Size(264, 45);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Stroom(W)";
+            this.label1.Text = "Weerstand(W)";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -82,7 +87,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(93, 245);
+            this.label2.Location = new System.Drawing.Point(17, 229);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(270, 45);
             this.label2.TabIndex = 3;
@@ -94,7 +99,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(93, 358);
+            this.label3.Location = new System.Drawing.Point(17, 321);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(274, 45);
             this.label3.TabIndex = 4;
@@ -103,18 +108,18 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(520, 216);
+            this.button1.Location = new System.Drawing.Point(215, 424);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(197, 50);
             this.button1.TabIndex = 8;
-            this.button1.Text = "Start Simulator";
+            this.button1.Text = "Start virtuele simulatie";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // numericUpDown1
             // 
             this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(101, 195);
+            this.numericUpDown1.Location = new System.Drawing.Point(25, 179);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
             this.numericUpDown1.TabIndex = 9;
@@ -128,7 +133,7 @@
             // numericUpDown2
             // 
             this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown2.Location = new System.Drawing.Point(101, 294);
+            this.numericUpDown2.Location = new System.Drawing.Point(25, 278);
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
             this.numericUpDown2.TabIndex = 10;
@@ -142,7 +147,7 @@
             // numericUpDown3
             // 
             this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown3.Location = new System.Drawing.Point(101, 406);
+            this.numericUpDown3.Location = new System.Drawing.Point(25, 379);
             this.numericUpDown3.Name = "numericUpDown3";
             this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
             this.numericUpDown3.TabIndex = 11;
@@ -156,19 +161,69 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(520, 278);
+            this.button2.Location = new System.Drawing.Point(12, 424);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(197, 50);
             this.button2.TabIndex = 12;
-            this.button2.Text = "Stop Simulator";
+            this.button2.Text = "Stop virtuele simulatie";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form1
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(477, 424);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 50);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Stop fysieke simulatie";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(680, 424);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(197, 50);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Start fysieke simulatie";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown4.Location = new System.Drawing.Point(487, 369);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown4.TabIndex = 16;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(479, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(264, 45);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Weerstand(W)";
+            // 
+            // BikeSimulator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(899, 502);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.numericUpDown3);
             this.Controls.Add(this.numericUpDown2);
@@ -179,12 +234,13 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.labelfietssimulator);
-            this.Name = "Form1";
+            this.Name = "BikeSimulator";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,6 +258,10 @@
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.NumericUpDown numericUpDown3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Remote Healthcare B3/BikeSimulator/BikeSimulator.cs
+++ Remote Healthcare B3/BikeSimulator/BikeSimulator.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Avans.TI.BLE;
+
 
 namespace FietsSimulatorGUI
 {
@@ -19,6 +21,7 @@ namespace FietsSimulatorGUI
         public int ValueHeartRate { get; set; }
 
         public Boolean StartButtonClicked { get; set; }
+        public Boolean StartButtonClickedv2 { get; set; }
 
         public int RandomNumber1 { get; set; }
         public int RandomNumber2 { get; set; }
@@ -39,6 +42,7 @@ namespace FietsSimulatorGUI
             ValueSpeed = 10;
             ValueHeartRate = 50;
             StartButtonClicked = false;
+            StartButtonClickedv2 = false;
             random = new Random();
             BikeData = new BikeData(ValueSpeed, ValueHeartRate, ValuePower, 10);
 
@@ -77,6 +81,29 @@ namespace FietsSimulatorGUI
             thread.Start();
         }
 
+        public void WorkThreadFunction()
+        {
+            try
+            {
+                while (StartButtonClicked)
+                {
+
+
+                    ValuePower = BikeData.GetPower();
+                    ValueSpeed = BikeData.GetSpeed();
+                    ValueHeartRate = BikeData.GetHeartRate();
+
+                    Console.WriteLine($"We are biking with the values  : {ValuePower} W , {ValueSpeed} m/s, {ValueHeartRate} bpm");
+                    Thread.Sleep(1000);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // log errors
+            }
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
 
@@ -102,26 +129,101 @@ namespace FietsSimulatorGUI
 
         }
 
-        public void WorkThreadFunction()
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StartButtonClickedv2 = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            System.Threading.Thread thread2 = new System.Threading.Thread(new ThreadStart(WorkThreadFunction2Async));
+            thread2.Start();
+
+        }
+
+        public async void WorkThreadFunction2Async()
         {
             try
             {
-                while(StartButtonClicked) {
-                    
-
-                    ValuePower = BikeData.GetPower();
-                    ValueSpeed = BikeData.GetSpeed();
-                    ValueHeartRate = BikeData.GetHeartRate();
-
-                    Console.WriteLine($"We are biking with the values  : {ValuePower} W , {ValueSpeed} m/s, {ValueHeartRate} bpm");
-                    Thread.Sleep(1000);
-                }
                 
-            }
+                    int errorCode = 0;
+                    BLE bleBike = new BLE();
+                    BLE bleHeart = new BLE();
+                    Thread.Sleep(1000); // We need some time to list available devices
+
+                    // List available devices
+                    List<String> bleBikeList = bleBike.ListDevices();
+                    Console.WriteLine("Devices found: ");
+                    foreach (var name in bleBikeList)
+                    {
+                        Console.WriteLine($"Device: {name}");
+                    }
+
+                    // Connecting
+                    errorCode = errorCode = await bleBike.OpenDevice(System.IO.File.ReadAllText(@"BikeBluetoothName.txt"));
+                    // __TODO__ Error check
+
+                    var services = bleBike.GetServices;
+                    foreach (var service in services)
+                    {
+                        Console.WriteLine($"Service: {service}");
+                    }
+
+                    // Set service
+                    errorCode = await bleBike.SetService("6e40fec1-b5a3-f393-e0a9-e50e24dcca9e");
+                    // __TODO__ error check
+
+                    // Subscribe
+                    bleBike.SubscriptionValueChanged += BleBike_SubscriptionValueChanged;
+                    errorCode = await bleBike.SubscribeToCharacteristic("6e40fec2-b5a3-f393-e0a9-e50e24dcca9e");
+
+                    // Heart rate
+                    errorCode = await bleHeart.OpenDevice(System.IO.File.ReadAllText(@"BikeBluetoothName.txt"));
+
+                    await bleHeart.SetService("HeartRate");
+
+                    bleHeart.SubscriptionValueChanged += BleBike_SubscriptionValueChanged;
+                    await bleHeart.SubscribeToCharacteristic("HeartRateMeasurement");
+
+
+                    Console.Read();
+                }
+
+            
             catch (Exception ex)
             {
                 // log errors
             }
+        }
+
+        private static void BleBike_SubscriptionValueChanged(object sender, BLESubscriptionValueChangedEventArgs e)
+        {
+
+            //foreach (byte b in e.Data)
+            //{
+            //    Console.Write(b + " ");
+            //}
+            if (e.ServiceName == "6e40fec2-b5a3-f393-e0a9-e50e24dcca9e")
+            {
+                if (e.Data[4] == 16)
+                {
+                    Console.WriteLine("\n\tSpeed: " + (e.Data[9] * 256 + e.Data[8]) / 1000.00 + "m/s");
+                    //Console.WriteLine("\telapsed time: " + e.Data[6]/4.0 + " seconds");
+                    //Console.WriteLine("\telapsed distance: " + e.Data[7] + " meters\n");
+
+                }
+            }
+            else if (e.ServiceName == "00002a37-0000-1000-8000-00805f9b34fb")
+            {
+                Console.WriteLine($"\n\tHeartRate: {e.Data[1]}bpm");
+            }
+
+            //Console.WriteLine("Received from {0}: {1}, {2}", e.ServiceName,
+            //    BitConverter.ToString(e.Data).Replace("-", " "),
+            //    Encoding.UTF8.GetString(e.Data));
         }
     }
 }
